e27dc72de9610ee32a0a190c520108d929efc390
Fernando Luna <337n4nd0@gmail.com>
2019-09-24 04:40:18 -0400

    added base ios project

---
+++ zomato-client-ios-app/zomato-client-ios-app/Modules/Root/View/RootViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RootViewController.swift
+//  zomato-client-ios-app
+//
+//  Created by Fernando Luna on 9/24/19.
+//  Copyright © 2019 Fernando Luna. All rights reserved.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
